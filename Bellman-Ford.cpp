4882a19a10f0ee33bdc7f6e3b5c4d14bd2476bb4
whybfq <13588119112@163.com>
2018-09-24 11:11:01 +0800

    Create Bellman-Ford.cpp

---
+++ Bellman-Ford.cpp
@@ -0,0 +1,56 @@
+/*********************************************************************************
+  *@copyright (C) 2018 san All Rights Reserved
+  *@file    Bellman-Ford.cpp
+  *@date    Sep 2018
+  *@author  san
+  *@CMAKE_CXX_STANDARD 17
+  *@IDE:    Clion 2018.3
+  *@OS:     macOS High Sierra 10.13.6
+  *
+  *@brief:
+  *@function_lists:
+  *  1.
+
+  *@ Time complexity: O(NM)
+  *@warning:
+  *@history:
+     1.Date:
+       Author:
+       Modification:
+**********************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+const int inf = 99999999; // use inf（infinity）to store a value which we consider positive infinity. 两个正无穷相加也小于2147483647(the max of int)
+
+int main() {
+    int dis[10],i,k,n,m,u[10],v[10],w[10];
+    // read n and m, N represents the number of vertices and M represents the number of edges.
+    cin >> n >> m;
+
+    // Read the edge
+    for (i = 1; i <= m ; i++) {
+        cin >> u[i] >> v[i] >> w[i];
+    }
+
+    // Initialization of dis array,This is the initial distance from vertex 1 to the rest of the vertices.
+    for(i = 1; i <= n; i++){
+        dis[i] = inf;
+    }
+    dis[1] = 0;
+
+    // core code of Bellman-Ford
+    for (k = 1; k <= n-1 ; k++) {
+        for (i = 1; i <= m ; i++) {
+            if(dis[v[i]] > dis[u[i]] + w[i])
+                dis[v[i]] = dis[u[i]] + w[i];
+        }
+    }
+
+    // output the result
+    for (i = 1; i <= n ; i++) {
+        cout << dis[i] << " ";
+    }
+}
