aec1d3214874e24339c564cb9dca627904ac2860
whybfq <13588119112@163.com>
2018-11-02 12:20:40 +0800

    Create StackUsingArray.cpp

---
+++ BasicDataStructure/Stack/StackUsingArray.cpp
@@ -0,0 +1,83 @@
+/*********************************************************************************
+  * @copyright (C)  2018 sanner All Rights Reserved
+  * @file: StackUsingArray.cpp {-bor}
+  * @brief:
+  *
+  * @date: Nov 2018
+  * @bug :
+  * @author: sanner
+  *
+  * @CMAKE_CXX_STANDARD 14
+  * @IDE: Clion 2018.2
+  * @OS:  macOS 10.14
+  *
+  * @function_lists:
+  *  1.  fg: void Swap(int A[] , int i, int j   -- exchange A[i] and A[j],dtype=int
+
+     2.
+  * @history:
+     1.@date:
+       @author:
+       @modification: Original
+     2.@date:
+       @author:
+       @modification:
+
+**********************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 100;
+
+class Stack {
+public:
+    int stack[MAX];
+    Stack() { top = -1; }
+    void push(int x);
+    void pop();
+    bool isEmpty();
+    void Print();
+private:
+    int top;
+};
+
+void Stack::push(int x) {
+    if(top >= (MAX-1)) {
+        cout << "Stack Overflow";
+    }
+    else {
+        stack[++top] = x;
+        cout << x << " pushed into stack\n";
+    }
+}
+
+
+void Stack::pop() {
+    if(top < 0) {
+        cout << "Stack Underflow";
+    }
+    else {
+        --top;
+    }
+}
+
+bool Stack::isEmpty() {
+    return (top < 0);
+}
+
+void Stack::Print() {
+    if(top == -1) { cout << "\nempty now" ; }
+    for (int i = 0; i <= top; ++i) {
+        cout << "Stack now: "<< stack[i] << " ";
+    }
+}
+
+int main(int argc, char* argv[]) {
+    struct Stack s;
+    s.push(10);  // 10 pushed into stack
+    s.Print();   // Stack noe: 10
+    s.pop();
+    s.Print();   // empty now
+}
