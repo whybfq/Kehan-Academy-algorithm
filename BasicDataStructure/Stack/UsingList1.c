8f224ffc3e31ad80ca3a40cbd8abdcb8286fd366
whybfq <13588119112@163.com>
2018-11-02 14:44:01 +0800

    Rename UsingList.c to UsingList1.c

---
+++ BasicDataStructure/Stack/UsingList1.c
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <stdlib.h>
+
+struct Node {   // a structure to represent a stack
+    int data;
+    struct Node *nextaddress;
+};
+
+struct Node *top = NULL;
+
+void Push(int x) {
+    printf("%d pushed into the stack\n", x);
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node*));  // malloc() nead headfile <stdlib.h>
+    temp->data = x;
+    temp->nextaddress = top;
+    top = temp;
+}
+
+void Pop() {
+    struct Node *temp;
+    if(top == NULL) {
+        printf("The stack is Empty now, can't pop any element\n");
+        return;   // stack is empty now
+    }
+    printf("%d popped from stack\n", top->data);
+    temp = top;
+    top = top->nextaddress;
+    free(temp);
+}
+
+//int IsEmpty(struct Node *top) {
+//    return !top;
+//}
+
+int main(void) {
+    Push(3);
+    Push(5);
+    Pop();
+    Pop();
+}
+
+
