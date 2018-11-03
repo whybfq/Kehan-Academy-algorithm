7c48f856e211995ebaffbaea302727be1e626bbe
whybfq <13588119112@163.com>
2018-11-03 15:39:37 +0800

    Create QueueUsingList.c

---
+++ BasicDataStructure/Queue/QueueUsingList.c
@@ -0,0 +1,69 @@
+/*********************************************************************************
+  * @copyright (C)  2018 sanner All Rights Reserved
+  * @file:  QueueUsingList.c   
+  * @brief: RingQueue if current position = i
+  *          next position = (i + 1) % N
+  *          previous position = (i - 1 + N) % N
+  *
+  * @date: Nov 2018
+  * @bug :
+  * @author: sanner
+  *
+  * @CMAKE_CXX_STANDARD 98
+  * @IDE: Clion 2018.2
+  * @OS:  macOS 10.14
+  *
+  * @function_lists:
+  *  1.  fg: void Swap(int A[] , int i, int j   -- exchange A[i] and A[j],dtype=int
+     2.
+  * @history:
+     1.@date:
+       @author:
+       @modification: Original
+     2.@date:
+       @author:
+       @modification:
+**********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node { // Queue - Liked list implementation
+    int data;
+    Node *next;
+};
+
+Node *front = NULL;
+Node *rear = NULL;
+
+void EnQueue(int x) {
+    Node *temp = (Node*)malloc(sizeof(Node));
+    printf( "%d enqueue \n",x);
+    temp->data = x;
+    temp->next = NULL;
+    if(front == NULL && rear == NULL) {
+        front = rear = temp;
+        return;
+    } else{
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+void DeQueue() {
+    Node* temp = front;
+    if(front == NULL) return;
+    if(front == rear) {
+        front = rear = NULL;
+    } else {
+        printf("%d dequeue\n",front->data);
+        front = front->next;
+    }
+    free(temp);
+}
+
+int main(int argc, char* argv[]) {
+    EnQueue(2); // 2 enqueue
+    EnQueue(6); // 6 enqueue
+    DeQueue();  // 2 dequeue
+}
+
