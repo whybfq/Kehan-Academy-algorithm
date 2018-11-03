d4490ec53651efa7a781242ec34b87416ea44ea9
whybfq <13588119112@163.com>
2018-11-03 10:48:54 +0800

    Update UsingList.cpp

--- BasicDataStructure/Stack/UsingList.cpp
+++ BasicDataStructure/Stack/UsingList.cpp
@@ -1,32 +1,63 @@
-#include <cstdio>
-#include <cstdlib>
+/*********************************************************************************
+  * @copyright (C)  2018 sanner All Rights Reserved
+  * @license: GPL
+  * @version:
+  * @file:
+  * @brief:
+  *
+  * @bug :
+  * @author: sanner
+  *
+  * @CMAKE_CXX_STANDARD 14
+  * @IDE: Clion 2018.2
+  * @OS:  macOS 10.14
+  *
+  * @function_lists:
+  *  1.  fg: void Swap(int A[] , int i, int j   -- exchange A[i] and A[j],dtype=int
+
+     2.
+  * @Revision:
+     1.@date:
+       @author:
+       @modification: Original
+     2.@date:
+       @author:
+       @modification:
+**********************************************************************************/
 #include <iostream>
 
-using namespace std;
+using std:: cout  ;
+using std:: endl  ;
+using std:: string;
 
 class  Node {   // a structure to represent a stack
 public:
+    Node() ;
     void Push(int x);
     void Pop();
     bool IsEmpty(){ return !top; }
 private:
     int data;
-    struct Node *nextaddress;
-    struct Node *top = NULL; //Null can also be the value of a pointer, which is the same as zero unless the CPU supports a special bit pattern for a null pointer.
-    // Note: In C, the null macro may have the type void* but this is not allowed in C++.
+    Node *nextaddress;
+    Node *top ; 
 };
 
+Node::Node() { 
+    Node *top = NULL; //Null can also be the value of a pointer, which is the same as zero unless the CPU supports a special bit pattern for a null pointer.
+    // Note: In C, the null macro may have the type void* but this is not allowed in C++.
+}
+
 void Node::Push(int x) {
+    Node *temp = new Node;  // malloc() nead headfile <stdlib.h>
     printf("%d pushed into the stack\n", x);
-    struct Node *temp = new(struct Node);  // malloc() nead headfile <stdlib.h>
     temp->data = x;
     temp->nextaddress = top;
     top = temp;
 }
 
 void Node::Pop() {
-    struct Node *temp;
-    if(top == NULL) {
+    Node *temp = new Node;
+    if(top == NULL) {  // if(IsEmpty())
         printf("The stack is Empty now, can't pop any element\n");
         return;   // stack is empty now
     }
@@ -38,11 +69,7 @@ void Node::Pop() {
 
 int main(void) {
     Node test;
-    test.Push(5);
     test.Push(3);
     test.Pop();
     test.Pop();
-    if(test.IsEmpty() ) {
-        cout << "empty" << endl;
-    }
 }
