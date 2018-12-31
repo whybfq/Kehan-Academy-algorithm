2fd3ebf0d72296936a0fe568285f1d6cae0adef7
whybfq <13588119112@163.com>
2018-12-31 22:35:15 +0700

    Update mergelist.cpp

--- BasicDataStructure/List/mergelist.cpp
+++ BasicDataStructure/List/mergelist.cpp
@@ -3,58 +3,56 @@
 using namespace std;
 
 typedef struct LNode {
-	int data; //½áµãµÄÊý¾ÝÓò
-	struct LNode *next; //½áµãµÄÖ¸ÕëÓò
-}LNode, *LinkList; //LinkListÎªÖ¸Ïò½á¹¹ÌåLNodeµÄÖ¸ÕëÀàÐÍ
+	int data;
+	struct LNode *next;
+}LNode, *LinkList;
 
-void CreateList_R(LinkList &L)//Î²²å·¨´´½¨µ¥Á´±í
+void CreateList_R(LinkList &L)
 {
-	//ÊäÈën¸öÔªËØµÄÖµ£¬½¨Á¢´ø±íÍ·½áµãµÄµ¥Á´±íL
 	int n;
 	LinkList s, r;
 	L=new LNode;
-	L->next=NULL; //ÏÈ½¨Á¢Ò»¸ö´øÍ·½áµãµÄ¿ÕÁ´±í
-	r=L; //Î²Ö¸ÕërÖ¸ÏòÍ·½áµã
+	L->next=NULL; 
+	r=L;
 	cout <<"ÇëÊäÈëÔªËØ¸öÊýn£º" <<endl;
 	cin>>n;
 	cout <<"ÇëÒÀ´ÎÊäÈën¸öÕûÐÍÊý(·ÇµÝ¼õ)£º" <<endl;
 	cout <<"Î²²å·¨´´½¨µ¥Á´±í..." <<endl;
 	while(n--)
     {
-		s=new LNode;//Éú³ÉÐÂ½áµã
-		cin>>s->data; //ÊäÈëÔªËØÖµ¸³¸øÐÂ½áµãµÄÊý¾ÝÓò
+		s=new LNode;
+		cin>>s->data;
 		s->next=NULL;
-		r->next=s;//½«ÐÂ½áµãs²åÈëÎ²½áµãrÖ®ºó
-		r=s;//rÖ¸ÏòÐÂµÄÎ²½áµãs
+		r->next=s;
+		r=s;
 	}
 }
 
 void mergelinklist(LinkList La, LinkList Lb, LinkList &Lc)
 {
     LinkList p,q,r;
-    p=La->next; //pÖ¸ÏòLaµÄµÚÒ»¸öÔªËØ
-    q=Lb->next; //qÖ¸ÏòLbµÄµÚÒ»¸öÔªËØ
-    Lc=La;      //LcÖ¸ÏòLaµÄÍ·½áµã
-    r=Lc;       //rÖ¸ÏòLcµÄÎ²²¿
+    p=La->next;
+    q=Lb->next;
+    Lc=La;     
+    r=Lc;     
     while(p&&q)
     {
-        if(p->data<=q->data)//°ÑpÖ¸ÏòµÄ½áµã´®ÆðÀ´
-        {
+        if(p->data<=q->data)        {
             r->next=p;
             r=p;
-            p=p->next;//pºóÒÆÒ»¸ö½áµã 
+            p=p->nex;
         }
-        else             //°ÑqÖ¸ÏòµÄ½áµã´®ÆðÀ´
+        else             
         {
             r->next=q;
             r=q;
             q=q->next;
         }
     }
-    r->next=p?p:q;//Ïàµ±ÓÚif(p) r->next=p; else r->next=q;
-    delete Lb;
+    r->next=p?p:q;
+
 }
-void Listprint_L(LinkList L) //µ¥Á´±íµÄÊä³ö
+void Listprint_L(LinkList L)
 {
     LinkList p;
     p=L->next;
