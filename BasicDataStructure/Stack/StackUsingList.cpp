bf8bddf770ac615f73f26d42d77b1629d3bf8ec6
whybfq <13588119112@163.com>
2019-01-05 11:13:05 +0700

    Update and rename UsingList.cpp to StackUsingList.cpp

---
+++ BasicDataStructure/Stack/StackUsingList.cpp
@@ -0,0 +1,33 @@
+/*********************************************************************************
+  * @copyright (C)  2018 sanner All Rights Reserved
+  * @license: GPL
+  * @version:
+  * @file:
+  * @brief:
+  *
+  * @bug :
+  * @author: sanner
+  *
+  * @CMAKE_CXX_STANDARD 14
+  * @IDE: Clion 2018.2
+  * @OS:  macOS 10.14
+  *
+  * @function_lists:
+  *  1.  fg: void Swap(int A[] , int i, int j   -- exchange A[i] and A[j],dtype=int
+
+     2.
+  * @Revision:
+     1.@date:
+       @author:
+       @modification: Original
+     2.@date:
+       @author:
+       @modification:
+**********************************************************************************/
+#include <iostream>
+
+using std:: cout  ;
+using std:: endl  ;
+using std:: string;
+
+  * @brief:
