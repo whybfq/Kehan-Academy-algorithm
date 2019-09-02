3e83145b017e46ba933d2639560074301e69a12e
whybfq <13588119112@163.com>
2019-09-02 10:07:42 +0700

    Update radixSort.py

--- radixSort.py
+++ radixSort.py
@@ -2,19 +2,22 @@
 
 
 def radix_sort(to_be_sorted):
-    maixmum_value = max(to_be_sorted)
+    maximum_value = max(to_be_sorted)
     max_exponent = len(str(maximum_value))
     being_sorted = to_be_sorted[:]
-
 
     for exponent in range(max_exponent):
         position = exponent + 1
         index = -position
+
         digits = [[] for i in range(10)]
 
         for number in being_sorted:
             number_as_a_string = str(number)
-            digit = number_as_a_string[index]
+            try :
+                digit = number_as_a_string[index]
+            except IndexError:
+                digit = 0
             digit = int(digit)
 
             digits[digit].append(number)
@@ -22,4 +25,9 @@ def radix_sort(to_be_sorted):
         being_sorted = []
         for numeral in digits:
             being_sorted.extend(numeral)
+
     return being_sorted
+
+unsorted_list = [839, 12, 30, 40]
+ordered_list = radix_sort(unsorted_list)
+print(ordered_list)
